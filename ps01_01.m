9e2deb9f25d862211c1e303dfd7fe32177690d06
moralesmendozar <moralesmendozar@gmail.com>
2019-11-15 20:32:09 -0500

    Add files via upload

--- ps01_01.m
+++ ps01_01.m
@@ -6,7 +6,7 @@
 % Based on Jesus Fernandez-Villaverde RBC comparison code
 % Okt 5, 2019
 
-% Problem Set 01. Exercise a,b,&c)
+% Problem Set 01. Exercise d)
 
 %% 0. Housekeeping
 
@@ -14,7 +14,6 @@ clear all
 close all
 clc
 
-tic
 
 %%  1. Calibration
 clear
@@ -29,7 +28,7 @@ aalpha =theta1;     % Elasticity of output w.r.t. capital
 theta2 = 0.6; % labor elasticity (cobbdouglas)
 W = 2;   % wage
 sigma = 0.1;  % std dev of eps (ar(1)) of log(a) (productivity)
-abar = 1.5; % log(abar) is the mean of log(a), which is ar(1)
+abar = 8; % log(abar) is the mean of log(a), which is ar(1)
 b0 = 0;    %parameter0 for cots of investing
 b1 = 0.5;   %parameter1 for cots of investing
 options = optimset('Display', 'off');  %when solves for kss
@@ -38,39 +37,54 @@ options = optimset('Display', 'off');  %when solves for kss
 na = 9;  %number of points for logaGrid = [loga0, loga1... loga9]
 logabar = log(abar);
 deltaLoga = sigma/sqrt(1-rho^2);  %change between each log(a);
-
-vProductivity = (logabar - deltaLoga*floor(na/2)):deltaLoga:(logabar + deltaLoga*floor(na/2));
+%%%%%%%%%%%%%%%%
+% For d), only take the meanshock 
+vProductivity = logabar;
+nGridProductivity = length(vProductivity);
 
 % Transition matrix
-P   = eye(na);
-a_1 = vProductivity(1);
-a_n = vProductivity(na);
-for j = 1:na
-    aj = vProductivity(j);
-    upperBoundA = (a_1 - rho*aj - (1-rho)*logabar +deltaLoga/2)/sigma;
-    P(1,j) = normcdf(upperBoundA);
-    lowerboundA = (a_n - rho*aj - (1-rho)*logabar -deltaLoga/2)/sigma;
-    P(na,j) = 1-normcdf(lowerboundA);
-end
-for i = 2:(na-1)
-    for j = 1:(na)
-        ai = vProductivity(i);
+if nGridProductivity > 1
+    P   = eye(na);
+    a_1 = vProductivity(1);
+    a_n = vProductivity(na);
+    for j = 1:na
         aj = vProductivity(j);
-        upperBoundA = (ai - rho*aj - (1-rho)*logabar +deltaLoga/2)/sigma;
-        lowerboundA = (ai - rho*aj - (1-rho)*logabar -deltaLoga/2)/sigma;
-        P(i,j) = normcdf(upperBoundA)-normcdf(lowerboundA);
+        upperBoundA = (a_1 - rho*aj - (1-rho)*logabar +deltaLoga/2)/sigma;
+        P(1,j) = normcdf(upperBoundA);
+        lowerboundA = (a_n - rho*aj - (1-rho)*logabar -deltaLoga/2)/sigma;
+        P(na,j) = 1-normcdf(lowerboundA);
     end
+    for i = 2:(na-1)
+        for j = 1:(na)
+            ai = vProductivity(i);
+            aj = vProductivity(j);
+            upperBoundA = (ai - rho*aj - (1-rho)*logabar +deltaLoga/2)/sigma;
+            lowerboundA = (ai - rho*aj - (1-rho)*logabar -deltaLoga/2)/sigma;
+            P(i,j) = normcdf(upperBoundA)-normcdf(lowerboundA);
+        end
+    end
+    P'
+    mTransition   = P;
+else
+    mTransition = 1;
 end
-P'
-mTransition   = P;
 
 
-% 2. Steady State
+%% 2. Steady State
 %define the functions of labor, profit and investment...
-labor = @(a,k) (theta2*(k.^theta1)'*a/W).^(1/(1-theta2));
+labor = @(a,k) (theta1*(k.^theta1)'*a/W).^(1/(1-theta2));
 profit = @(a,k) ((k'.^theta1)*a).*(labor(a,k).^theta2) - W*labor(a,k);
 investment = @(a,k,kprime) kprime - (1-delta)* k'*ones(size(a,1));
-phi = @(a,k,kprime) b0 * k'*ones(1,size(a,1)) + b1*(( investment(a,k,kprime)./ ( k'*ones(1,size(a,1)) )- delta).^2).*k'*ones(1,size(a,1));
+%for d) 1 and 2
+phi = @(a,k,kprime) b0 * k'*ones(1,size(a,1)) +...
+    b1*(( investment(a,k,kprime)./ ( k'*ones(1,size(a,1)) )- delta).^2).*k'*ones(1,size(a,1));
+%for d) (3) ,  b1 =10  if investment is higher than delta*k
+phi2 = @(a,k,kprime) b0 * k'*ones(1,size(a,1)) +...
+     10*(( investment(a,k,kprime)./ ( k'*ones(1,size(a,1)) )-delta).^2).*k'*ones(1,size(a,1));
+%for d) (4)
+%phi3 = @(a,k,kprime) b0 * k'*ones(1,size(a,1)) +...
+%     b1*(( investment(a,k,kprime)./ ( k'*ones(1,size(a,1)) )-
+%     delta).^2).*k'*ones(1,size(a,1));
 
 %find the values of the steady state:
 fss =@(a,kss) delta + b0 - (theta1*a*kss^(theta1-1)*labor(a,kss)^theta2)/(1+r);
@@ -82,7 +96,7 @@ fprintf('\n')
 
 % with kss, generate the grid of capital
 capMiddle = (aalpha*abar/(r+delta))^(1/(1-aalpha));
-kstep = 0.00001; %0.00001
+kstep = 0.1; %0.00001
 %vGridCapital = 0.5*capitalSteadyState:kstep:1.5*capitalSteadyState;
 vGridCapital = 0.5*capMiddle:kstep:1.5*capMiddle;
 kapitalMax = (abar/(delta))^(1/(1-aalpha));
@@ -121,12 +135,12 @@ profitMatrix = profit(exp(vProductivity),vGridCapital);
 maxDifference = 10.0;
 tolerance = 0.00001;%0.0000001;
 iteration = 0;
-
+tic
 while (maxDifference>tolerance)  
     
     expectedValueFunction = mValueFunction*mTransition';
     
-    parfor nProductivity = 1:nGridProductivity
+    for nProductivity = 1:nGridProductivity
         
         % We start from previous choice (monotonicity of policy function)
         gridCapitalNextPeriod = 1;
@@ -142,7 +156,13 @@ while (maxDifference>tolerance)
                 prodctvt = exp(vProductivity(nProductivity));
                 currentCapital = vGridCapital(nCapital);
                 kprimeTomorow = vGridCapital(nCapitalNextPeriod);
-                dividend = profitMatrix(nCapital,nProductivity) - investment(prodctvt,currentCapital,kprimeTomorow) - phi(prodctvt,currentCapital,kprimeTomorow);
+                invstm = investment(prodctvt,currentCapital,kprimeTomorow);
+                %for 01.(d)(3)
+                if invstm > delta*currentCapital
+                    dividend = profitMatrix(nCapital,nProductivity) - invstm - phi(prodctvt,currentCapital,kprimeTomorow);
+                else
+                    dividend = profitMatrix(nCapital,nProductivity) - invstm - phi2(prodctvt,currentCapital,kprimeTomorow);
+                end
                 valueProvisional = (1-bbeta)*dividend+bbeta*expectedValueFunction(nCapitalNextPeriod,nProductivity);
             
                 if (valueProvisional>valueHighSoFar)
@@ -184,14 +204,13 @@ toc
 figure(2)
 
 subplot(2,1,1)
-xkap = vGridCapital(1:nGridCapital/2);
-plot(xkap,mValueFunction(1:nGridCapital/2,:))
+plot(vGridCapital(1:100),mValueFunction(1:100,:))
 xlim([vGridCapital(1) vGridCapital(nGridCapital/2)])
 xlabel('k')
 title('Value Function')
 
 subplot(2,1,2)
-plot(xkap,mPolicyFunction(1:nGridCapital/2,:))
+plot(vGridCapital(1:100),mPolicyFunction(1:100,:))
 xlim([vGridCapital(1) vGridCapital(nGridCapital/2)])
 xlabel('k')
 title('Policy Function')
@@ -199,7 +218,7 @@ title('Policy Function')
 % vExactPolicyFunction = aalpha*bbeta.*(vGridCapital.^aalpha);
 % 
 % subplot(3,1,3)
-% plot((100.*(vExactPolicyFunction'-mPolicyFunction(:,3))./mPolicyFunction(:,3)))
+% plot((100.*(vExactPolicyFunction'-mPolicyFunction(:,1))./mPolicyFunction(:,1)))
 % title('Comparison of Exact and Approximated Policy Function')
 
 %set(gcf,'PaperOrientation','landscape','PaperPosition',...
@@ -224,33 +243,24 @@ for nProductivity = 1:nGridProductivity
 end
 %shock a
 figure(3)
-subplot(2,1,1)
-xkap = vGridCapital(1:nGridCapital/2);
-plot(xkap,mOptimalInvestment(1:nGridCapital/2,8))
+% subplot(2,1,1)
+plot(vGridCapital(1:100),mOptimalInvestment(1:100,1))
 xlabel('k')
-title('Optimal Invesment')
-hold on
-plot(xkap,mOptimalInvestment(1:nGridCapital/2,5))
-plot(xkap,mOptimalInvestment(1:nGridCapital/2,1))
-legend('high a', 'middle a', 'low a')
-hold off
+title('d)01) Optimal Invesment for the mean shock only')
 
-subplot(2,1,2)
-plot(xkap,mFinancing(1:nGridCapital/2,8))
-xlabel('k')
-title('Financing (-d(a,k) when positive)')
-hold on
-plot(xkap,mFinancing(1:nGridCapital/2,5))
-plot(xkap,mFinancing(1:nGridCapital/2,1))
-legend('high a', 'middle a', 'low a')
-hold off
+% hold on
+% plot(vGridCapital,mOptimalInvestment(:,5))
+% plot(vGridCapital,mOptimalInvestment(:,1))
+% legend('high a', 'middle a', 'low a')
+% hold off
 
-figure(4)
-plot(xkap,mOptimalInvestment(1:nGridCapital/2,8))
-xlabel('k')
-title('Optimal Invesment')
-hold on
-plot(xkap,mOptimalInvestment(1:nGridCapital/2,5))
-plot(xkap,mOptimalInvestment(1:nGridCapital/2,1))
-legend('high a', 'middle a', 'low a')
-hold off
+% subplot(2,1,2)
+% plot(vGridCapital(1:100),mFinancing(1:100,1))
+% xlabel('k')
+% title('d) Financing (-d(a,k) when positive) for the mean shock only')
+
+% hold on
+% plot(vGridCapital,mFinancing(:,5))
+% plot(vGridCapital,mFinancing(:,1))
+% legend('high a', 'middle a', 'low a')
+% hold off
